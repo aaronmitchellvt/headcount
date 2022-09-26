a1460dab972b4a88f1140597eff5cd7de0a887c0
aaron mitchell <aaronmitchell9902@gmail.com>
2022-09-25 23:24:42 -0400

    moving-index

--- server/src/config/getClientIndexPath.js
+++ server/src/config/getClientIndexPath.js
@@ -3,10 +3,10 @@ import path, { dirname } from "path";
 import config from "../config.js";
 
 const getClientIndexPath = () => {
-  const currentPath = dirname(fileURLToPath(import.meta.url));
-  let indexPath = path.join(currentPath, "../../public/dist/index.html");
+  const currentPath = dirname(fileURLToPath(meta.url));
+  let indexPath = path.join(currentPath, "index.html");
   if (config.nodeEnv !== "production") {
-    indexPath = path.join(currentPath, "../../../client/public/index.html");
+    indexPath = path.join(currentPath, "index.html");
   }
 
   return indexPath;
