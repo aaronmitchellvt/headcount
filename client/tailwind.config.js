b863d290b322484f8ce7e2ee77bb0bd03ab37225
aaron mitchell <aaronmitchell9902@gmail.com>
2022-09-30 00:12:30 -0400

    Revert "css-3"
    This reverts commit b09bd1c03163a6a85598b2c4f0775e871db8dded.

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,62 +1,39 @@
-
-const tailwindcss = require("tailwindcss");
+// const tailwindcss = require("tailwindcss");
+/** @type {import('tailwindcss').Config} */
+// import head from './src/components/authentication/'
 
 module.exports = {
+  node: "jit",
   content: ["./src/components/**/*.js"],
+  purge: {
+    enabled: true,
+    content: ["./src/components/**/*.js","./public/index.html"],
+  },
   theme: {
-    screens: {
-      xs: "400px",
-      sm: "480px",
-      md: "740px",
-      lg: "976px",
-      xl: "1440px",
+    colors: {
+      'blue': '#1fb6ff',
+      'purple': '#7e5bef',
+      'pink': '#ff49db',
+      'orange': '#ff7849',
+      'green': '#13ce66',
+      'yellow': '#ffc82c',
+      'gray-dark': '#273444',
+      'gray': '#8492a6',
+      'gray-light': '#d3dce6',
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
     },
     extend: {
-      fontFamily: {
-        Lobster: ["Lobster", "cursive"]
+      spacing: {
+        '8xl': '96rem',
+        '9xl': '128rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
       }
-    },
+    }
   },
-  plugins: [
-    require("@tailwindcss/aspect-ratio")
-  ],
+  plugins: [],
 }
-// // const tailwindcss = require("tailwindcss");
-// /** @type {import('tailwindcss').Config} */
-// // import head from './src/components/**/*.js'
-
-// module.exports = {
-//   node: "jit",
-//   content: ["./src/components/**/*.js"],
-//   purge: {
-//     enabled: true,
-//     content: ['./src/components/**/*.js','./public/index.html'],
-//   },
-//   theme: {
-//     colors: {
-//       'blue': '#1fb6ff',
-//       'purple': '#7e5bef',
-//       'pink': '#ff49db',
-//       'orange': '#ff7849',
-//       'green': '#13ce66',
-//       'yellow': '#ffc82c',
-//       'gray-dark': '#273444',
-//       'gray': '#8492a6',
-//       'gray-light': '#d3dce6',
-//     },
-//     fontFamily: {
-//       sans: ['Graphik', 'sans-serif'],
-//       serif: ['Merriweather', 'serif'],
-//     },
-//     extend: {
-//       spacing: {
-//         '8xl': '96rem',
-//         '9xl': '128rem',
-//       },
-//       borderRadius: {
-//         '4xl': '2rem',
-//       }
-//     }
-//   },
-//   plugins: [],
-// }
